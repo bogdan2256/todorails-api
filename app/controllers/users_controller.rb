2f09013bc8118829eda802bbaac85ae1b5dd2268
Bogdan <bogdan__sh@ukr.net>
2018-05-10 17:37:58 +0300

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
 
-    if user&user.valid? && user.save
+    if user&.valid? && user.save
       render json:  {message: 'user created, now confirm your email'}
     else
       render json: {errors: user.errors.full_messages.to_sentence}
