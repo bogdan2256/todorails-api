7a25b41367a7b664967aef8bbfa2a71cdcd12e19
Bogdan <bogdan__sh@ukr.net>
2018-05-10 17:26:46 +0300

    delete binding pry

--- app/controllers/email_confirmation_controller.rb
+++ app/controllers/email_confirmation_controller.rb
@@ -3,7 +3,6 @@ class EmailConfirmationController < ApplicationController
   expose :user, -> {User.find_by(token: token)}
 
   def show
-    binding.pry
     redirect_to "#{Figaro.env.frontend_url}/users/sign_in" if user&user.update_column(:email_confirmed, true)
   end
 end
