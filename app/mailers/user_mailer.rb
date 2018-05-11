9a396313009c692d85235b041796763442bcdefc
Bogdan <bogdan__sh@ukr.net>
2018-05-11 14:02:37 +0300

    #14 user mailer(correction)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
   def registration_confirmation(user)
     @user = user
-    @email = user.email
     mail(to: @user.email, subject: 'Email confirmation')
   end
 end
