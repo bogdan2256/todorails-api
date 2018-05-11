651ebec4cde8ddf86a4cc4938d93461387919ec3
Bogdan <bogdan__sh@ukr.net>
2018-05-11 12:58:42 +0300

    #13 validation rules

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true,
+    length: {
+      minimum: 4,
+      maximum: 10
+    }
+
+  validates :description, presence: true,
+    length: {
+      minimum: 4,
+      maximum: 10
+    }
+  validates :priority, presence: true
 end
