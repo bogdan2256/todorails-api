27f9dacbce30408dfc77d929361054ab8f358a8a
Bogdan <bogdan__sh@ukr.net>
2018-05-17 12:40:55 +0300

    add priority for serializer(sort by priority(fix))

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::API
     current_user ||= User.find_by(token: request.headers['Authorization'])
   end
 
-  def render_api(object, status = :ok, options = {})
-    if object.respond_to?(:errors) && object.errors.present?
-      render json: { message: object.errors.full_messages.to_sentence },
-             status: :unprocessable_entity
-    else
-      render({ json: object, status: status }.merge(options))
-    end
-  end
+  # def render_api(object, status = :ok, options = {})
+  #   if object.respond_to?(:errors) && object.errors.present?
+  #     render json: { message: object.errors.full_messages.to_sentence },
+  #            status: :unprocessable_entity
+  #   else
+  #     render({ json: object, status: status }.merge(options))
+  #   end
+  # end
 
   private
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     return render json: { message: 'Invalid email or password.' }, status: :unprocessable_entity unless user&.authenticate(session_params[:password])
     return render json: { message: 'Confirm your email.' }, status: :unprocessable_entity unless user.email_confirmed
     render json: userq, status: 200, each_serializer: SessionSerializer
+    # render_api(userq, :ok, each_serializer: SessionSerializer)
   end
 
   private
