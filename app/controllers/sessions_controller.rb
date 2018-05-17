97a6523631012c059ce5cea31c5906bbcfde7756
Bogdan <bogdan__sh@ukr.net>
2018-05-17 13:56:04 +0300

    #10 session params(correction)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by_email(session_params[:email].downcase)
+    user = User.find_by_email(params[:session][:email].downcase)
     userq = {email: user.email, token: user.token, full_name: user.full_name, username: user.username}
-    return render json: { message: 'Invalid email or password.' }, status: :unprocessable_entity unless user&.authenticate(session_params[:password])
+    return render json: { message: 'Invalid email or password.' }, status: :unprocessable_entity unless user&.authenticate(params[:session][:password])
     return render json: { message: 'Confirm your email.' }, status: :unprocessable_entity unless user.email_confirmed
     render json: userq, status: 200, each_serializer: SessionSerializer
-    # render_api(userq, :ok, each_serializer: SessionSerializer)
-  end
-
-  private
-
-  def session_params
-    params.require(:session).permit(:email, :password)
   end
 end
