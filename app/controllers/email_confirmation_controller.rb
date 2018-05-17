15f383ad828fc6ca6ff92af492dba039d3ea8105
Bogdan <bogdan__sh@ukr.net>
2018-05-17 14:19:38 +0300

    #7 confirmation_controller(correction)

--- app/controllers/email_confirmation_controller.rb
+++ app/controllers/email_confirmation_controller.rb
@@ -1,6 +1,6 @@
 class EmailConfirmationController < ApplicationController
-  expose :token, -> {params[:format]}
-  expose :user, -> {User.find_by(token: token)}
+  # expose :token, -> {params[:format]}
+  expose :user, -> {User.find_by(token: params[:format])}
 
   def show
     redirect_to "#{Figaro.env.frontend_url}/users/sign_in" if user && user.update_column(:email_confirmed, true)
